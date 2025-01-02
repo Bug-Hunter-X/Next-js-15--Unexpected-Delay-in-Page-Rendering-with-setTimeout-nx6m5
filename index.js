fef3ae802dc37b1f9b9ae27fc37f5e6880782e5b
BugHunter-X <askzenai@gmail.com>
2025-01-02 07:42:06 +0100

    Add index.js file

---
+++ index.js
@@ -0,0 +1,13 @@
+```javascript
+// pages/about.js
+
+export default function About() {
+  //The setTimeout is removed.
+  console.log('About page rendered');
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
+```
